46b3b28d302310b2894da285e7cee88a9eeef1f5
Gianluca Scopelliti <gianluca.scopelliti@ericsson.com>
2022-02-01 09:26:46 +0000

    fix OPTEE version

--- scripts/install_trustzone.sh
+++ scripts/install_trustzone.sh
@@ -2,12 +2,14 @@
 
 set -eux
 
+OPTEE_VERSION=3.15.0
+
 # toolchain
-git clone https://github.com/OP-TEE/build.git
+git clone --branch $OPTEE_VERSION https://github.com/OP-TEE/build.git
 (cd build && make -f toolchain.mk -j2)
 
 # optee_os
-git clone https://github.com/OP-TEE/optee_os
+git clone --branch $OPTEE_VERSION https://github.com/OP-TEE/optee_os
 (cd optee_os && make \
   CFG_TEE_BENCHMARK=n \
   CFG_TEE_CORE_LOG_LEVEL=3 \
@@ -20,10 +22,10 @@ git clone https://github.com/OP-TEE/optee_os
   PLATFORM=vexpress-qemu_virt)
 
 # optee_client
-git clone https://github.com/OP-TEE/optee_client
+git clone --branch $OPTEE_VERSION https://github.com/OP-TEE/optee_client
 (cd optee_client && mkdir build && cd build \
   && cmake -DCMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -DCMAKE_INSTALL_PREFIX=../out/export/usr .. \
   && make && make install)
 
 # optee_examples
-git clone https://github.com/linaro-swg/optee_examples.git
+git clone --branch $OPTEE_VERSION https://github.com/linaro-swg/optee_examples.git
