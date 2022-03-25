3cd06f863600cf41a968d7586536d91dd388dcef
Gianluca Scopelliti <gianlu.1033@gmail.com>
2022-03-25 08:13:36 -0700

    Revert "use upstream sancus-compiler"
    This reverts commit 4e4eddaee0a2043e1c1112476ede737ab91ee943.

--- scripts/install_sancus.sh
+++ scripts/install_sancus.sh
@@ -18,6 +18,10 @@ make install clean SANCUS_SECURITY=$1 SANCUS_KEY=$2
 
 cd ..
 
+# patch sancus-compiler to remove Aion-related commits (Sancus' EM not compatible yet)
+git clone --branch no-aion https://github.com/AuthenticExecution/sancus-compiler.git
+make_repo "sancus-compiler" $1 $2
+
 # patch sancus-support due to Riot bugs
 # TODO remove this as soon as the bug is fixed
 git clone https://github.com/AuthenticExecution/sancus-support.git
